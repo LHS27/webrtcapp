c94b4ae145bab4d74d6fe00d2b24a15df243fbbf
LHS27 <59645974+LHS27@users.noreply.github.com>
2020-01-28 11:21:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -113,14 +113,13 @@ function updateBtn() {
     navigator.mediaDevices.getUserMedia.video = false;
   } else {
     button.value = 'Arrêter la caméra';
-   constraints.video =  facingMode:{ exact = 'environment' };
-   navigator.mediaDevices.getUserMedia.video = facingMode:{ exact = 'environment' };
-
+   constraints.video =  facingMode: { exact = 'environment' };
+   navigator.mediaDevices.getUserMedia.video = facingMode: { exact = 'environment' };
   }
 }
 navigator.mediaDevices.getUserMedia({
   audio: false,
-video: {  facingMode:{ exact = 'environment' } } })
+video: facingMode:{ exact = 'environment' } })
 
 .then(gotStream)
 .catch(function(e) {
